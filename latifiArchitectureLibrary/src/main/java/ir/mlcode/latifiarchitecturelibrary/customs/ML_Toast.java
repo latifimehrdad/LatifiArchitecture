6c0b4e865f1c62b9a91962e8ae7d0d27820d8801
980902 <m3.latifi@gmail.com>
2020-10-21 15:15:26 +0330

    تغییر آیکونeditText

--- latifiArchitectureLibrary/src/main/java/ir/mlcode/latifiarchitecturelibrary/customs/ML_Toast.java
+++ latifiArchitectureLibrary/src/main/java/ir/mlcode/latifiarchitecturelibrary/customs/ML_Toast.java
@@ -147,8 +147,8 @@ public class ML_Toast extends LinearLayout {
         else
             titleLength = 1;
         delay = 1000 * titleLength;
-        if (delay < 1000)
-            delay = 2000;
+        if (delay < 1500)
+            delay = 2500;
         Handler handler = new Handler();
         handler.postDelayed(() -> {
             view.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide_out_left));
