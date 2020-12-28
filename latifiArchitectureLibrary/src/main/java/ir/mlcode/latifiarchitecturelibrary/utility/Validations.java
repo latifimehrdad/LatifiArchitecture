a09433008c2df2690b84573cd3204bbc2c7ae6ba
Mehrdad <m3.latifi@gmail.com>
2020-12-28 20:15:28 +0330

    اعتبار سنجی کد ملی

--- latifiArchitectureLibrary/src/main/java/ir/mlcode/latifiarchitecturelibrary/utility/Validations.java
+++ latifiArchitectureLibrary/src/main/java/ir/mlcode/latifiarchitecturelibrary/utility/Validations.java
@@ -74,8 +74,27 @@ public class Validations {
         if (national.length() != 10)
             return false;
 
-        return true;
+        long nationalCode = Long.parseLong(national);
+        byte[] arrayNationalCode = new byte[10];
+
+        //extract digits from number
+        for (int i = 0; i < 10 ; i++) {
+            arrayNationalCode[i] = (byte) (nationalCode % 10);
+            nationalCode = nationalCode / 10;
+        }
+
+        //Checking the control digit
+        int sum = 0;
+        for (int i = 9; i > 0 ; i--)
+            sum += arrayNationalCode[i] * (i+1);
+        int temp = sum % 11;
+        if (temp < 2)
+            return arrayNationalCode[0] == temp;
+        else
+            return arrayNationalCode[0] == 11 - temp;
+
     }
     //______________________________________________________________________________________________ nationalValidation
+
 
 }
